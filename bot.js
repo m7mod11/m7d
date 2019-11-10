734eff65a0607b422c13e282a5bb95e2d437569d
m7mod11 <57415430+m7mod11@users.noreply.github.com>
2019-11-10 02:18:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ client.on('message', message => {
       message.channel.send(`**✅   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(Arab Hawks Network, "https://www.twitch.tv/dream");
+    client.user.setGame(,Arab Hawks Network, "https://www.twitch.tv/dream");
       message.channel.send(`**✅**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
